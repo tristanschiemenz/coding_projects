5716a687e7c025f59df89578fce1900fc7fce264
Tristan Schiemenz <tristanschiemenz@gmail.com>
2023-02-06 22:26:10 +0100

    06.02.23

--- c++/learning/main.cpp
+++ c++/learning/main.cpp
@@ -1,16 +1,42 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <string>
 
+
+
+
+int sumup(int *eins, int zwei){
+    *eins += zwei;
+}
 int main(){
 
-    int age;
+    int zahl = 10;
+    std::string str = "Hello";
+    std::cout << zahl << std::endl;
+    std::cout << str << std::endl;
 
-    std::cout << "Enter your age: ";
-    std::cin >> age;
+    int &refzahl = zahl; //referenz aber leitet refzahl zu zahl um
+    std::cout << refzahl << std::endl;
+    int* ptrzahl = &zahl;
+    int eins;
+    int zwei;
+    int drei;
+    std::cout << &eins << " " << &zwei << " " << &drei << std::endl; 
+    std::cout << ptrzahl << " ";
+    *ptrzahl += 27;
+    std::cout << ptrzahl << " \n";
+    std::cout << *ptrzahl << std::endl;
 
-    if(age>=18){
-        std::cout << "You are in";
+    int &test = *ptrzahl;
+    zahl += 1;
+    std::cout << test << std::endl;
 
-    } 
+    int haupt = 10;
+    int dazu = 15;
+    sumup(&haupt,dazu);
+    std::cout << haupt << std::endl;
+    
+
+    return 0;
 }
