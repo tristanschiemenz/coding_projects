f791b6d723b36522a19c3f2a54ac05e4f4aff9b9
Tristan Schiemenz <tristanschiemenz@gmail.com>
2023-04-07 18:01:55 +0200

    commenting and couningsort update
    Commenting:
    Enviormental Issue because of device switching
    Counting Sort Update:
    finding the highest by it self and looping with blue and red marker over the array

--- c++/SDL2/visualization/main.cpp
+++ c++/SDL2/visualization/main.cpp
@@ -1,4 +1,4 @@
-#include <SDL2/SDL.h>
+// #include <SDL2/SDL.h>
 #include <iostream>
 #include <random>
 #include <vector>
@@ -14,8 +14,8 @@ class SortingAlgs{
         Sdrawing = drawing;
 
         if(Sdrawing){
-            SDL_CreateWindowAndRenderer(n*scaling,n*scaling,0,&window,&renderer);
-            SDL_RenderSetScale(renderer,scaling,scaling);
+            // SDL_CreateWindowAndRenderer(n*scaling,n*scaling,0,&window,&renderer);
+            // SDL_RenderSetScale(renderer,scaling,scaling);
         }
     }
     void print_array(std::vector<int> &v){
@@ -33,12 +33,26 @@ class SortingAlgs{
                     //Draw
                     draw(v,i,j);
                     //Show
-                    SDL_Delay(delay);
+                    // SDL_Delay(delay);
                 }
             }
         }
     }
-    void counting_sort(std::vector<int> &v,const int heighest,int delay){
+    void counting_sort(std::vector<int> &v,int delay){
+        //finding the heighes value
+        int indexHeighest = 0;
+        int heighest = v[indexHeighest]; 
+        for(int i = 1; i < v.size(); i++){
+            if(v[i] > heighest){
+                heighest = v[i];
+                indexHeighest = i;
+            }
+            if(Sdrawing){
+                draw(v,indexHeighest,i);
+                // SDL_Delay(delay)
+            }
+        }
+        
         int counts[heighest+1];
             // Initialize the count for each element in array to 0
         for (int i = 0; i <= heighest+1; ++i) {
@@ -51,7 +65,7 @@ class SortingAlgs{
                     //Draw
                     draw(v,i,i);
                     //Delay
-                    SDL_Delay(delay);
+                    // SDL_Delay(delay);
 
             }
         }
@@ -63,34 +77,32 @@ class SortingAlgs{
                 position++;
                 if(Sdrawing){
                     draw(v,i,i);
-                    SDL_Delay(delay);
+                    // SDL_Delay(delay);
                 }
             }
         }
-
-
-
     }
+    void insertion_sort(std::vector<int> &v){}
     private:
     void draw(std::vector<int> &v, unsigned int red,unsigned int blue){
-        SDL_SetRenderDrawColor(renderer,0,0,0,255);
-        SDL_RenderClear(renderer);
+        // SDL_SetRenderDrawColor(renderer,0,0,0,255);
+        // SDL_RenderClear(renderer);
         for(int i = 0;i<v.size();i++){
             if(i == red){
-                SDL_SetRenderDrawColor(renderer,255,0,0,255);
+                // SDL_SetRenderDrawColor(renderer,255,0,0,255);
             }else if (i == blue){
-                SDL_SetRenderDrawColor(renderer,0,0,255,255);
+                // SDL_SetRenderDrawColor(renderer,0,0,255,255);
             }else{
-                SDL_SetRenderDrawColor(renderer,255,255,255,255);
+                // SDL_SetRenderDrawColor(renderer,255,255,255,255);
             }
             
-            SDL_RenderDrawLine(renderer,i,n,i,n-v[i]);
+            // SDL_RenderDrawLine(renderer,i,n,i,n-v[i]);
         }
-        SDL_RenderPresent(renderer);
+        // SDL_RenderPresent(renderer);
 }
     bool Sdrawing;
-    SDL_Window* window = nullptr;
-    SDL_Renderer* renderer = nullptr;
+    // SDL_Window* window = nullptr;
+    // SDL_Renderer* renderer = nullptr;
 
 
 };
@@ -109,13 +121,13 @@ int main(int argc, char *argv[]){
 
     // Shuffle the vector
     std::shuffle(v.begin(), v.end(), rng);
-    bool drawing = true;
+    bool drawing = false;
     SortingAlgs sorting(drawing,6);
     // sorting.bubble_sort(v,1);
 
     // std::shuffle(v.begin(), v.end(), rng);
 
-    sorting.counting_sort(v,n,0);
+    sorting.counting_sort(v,0);
     sorting.print_array(v);
     
     //Returning
