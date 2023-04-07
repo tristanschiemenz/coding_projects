241831da61489d2e162de44ff18ca1b93ed9e26a
Tristan Schiemenz <tristanschiemenz@gmail.com>
2023-04-07 16:24:27 +0200

    makeing it a class and adding counting sort

--- c++/SDL2/visualization/main.cpp
+++ c++/SDL2/visualization/main.cpp
@@ -7,20 +7,93 @@
 
 const int n = 100;
 
-void draw(std::vector<int>& v, SDL_Renderer* renderer, unsigned int red,unsigned int blue){
-    int index = 0;
-    for(int i = 0;i<v.size();i++){
-        if(i == red){
-            SDL_SetRenderDrawColor(renderer,255,0,0,255);
-        }else if (i == blue){
-            SDL_SetRenderDrawColor(renderer,0,0,255,255);
-        }else{
-            SDL_SetRenderDrawColor(renderer,255,255,255,255);
+
+class SortingAlgs{
+    public:
+    SortingAlgs(bool &drawing, int scaling){
+        Sdrawing = drawing;
+
+        if(Sdrawing){
+            SDL_CreateWindowAndRenderer(n*scaling,n*scaling,0,&window,&renderer);
+            SDL_RenderSetScale(renderer,scaling,scaling);
         }
-        
-        SDL_RenderDrawLine(renderer,i,n,i,n-v[i]);
     }
+    void print_array(std::vector<int> &v){
+        for(int i = 0; i<v.size(); i++){
+            std::cout << v[i] << " ";
+        }
+    }
+    void bubble_sort(std::vector<int>& v, int delay){
+        for(int i= 1; i< v.size();i++){
+            for(int j = 0; j<v.size();j++){
+                if(v[i] < v[j]){
+                    std::swap(v[i],v[j]);
+                }
+                if(Sdrawing){
+                    //Draw
+                    draw(v,i,j);
+                    //Show
+                    SDL_Delay(delay);
+                }
+            }
+        }
+    }
+    void counting_sort(std::vector<int> &v,const int heighest,int delay){
+        int counts[heighest+1];
+            // Initialize the count for each element in array to 0
+        for (int i = 0; i <= heighest+1; ++i) {
+            counts[i] = 0;
+        }
+        //Counting
+        for(int i=0;i<v.size();i++){
+            counts[v[i]]++;
+            if(Sdrawing){
+                    //Draw
+                    draw(v,i,i);
+                    //Delay
+                    SDL_Delay(delay);
+
+            }
+        }
+        //Writing the new results
+        int position = 0;
+        for(int i = 0;i < heighest + 1;i++){
+            for(int j = 0; j < counts[i]; j++){
+                v[position] = i;
+                position++;
+                if(Sdrawing){
+                    draw(v,i,i);
+                    SDL_Delay(delay);
+                }
+            }
+        }
+
+
+
+    }
+    private:
+    void draw(std::vector<int> &v, unsigned int red,unsigned int blue){
+        SDL_SetRenderDrawColor(renderer,0,0,0,255);
+        SDL_RenderClear(renderer);
+        for(int i = 0;i<v.size();i++){
+            if(i == red){
+                SDL_SetRenderDrawColor(renderer,255,0,0,255);
+            }else if (i == blue){
+                SDL_SetRenderDrawColor(renderer,0,0,255,255);
+            }else{
+                SDL_SetRenderDrawColor(renderer,255,255,255,255);
+            }
+            
+            SDL_RenderDrawLine(renderer,i,n,i,n-v[i]);
+        }
+        SDL_RenderPresent(renderer);
 }
+    bool Sdrawing;
+    SDL_Window* window = nullptr;
+    SDL_Renderer* renderer = nullptr;
+
+
+};
 
 int main(int argc, char *argv[]){
 
@@ -36,33 +109,15 @@ int main(int argc, char *argv[]){
 
     // Shuffle the vector
     std::shuffle(v.begin(), v.end(), rng);
+    bool drawing = true;
+    SortingAlgs sorting(drawing,6);
+    // sorting.bubble_sort(v,1);
 
-    //Window and Renderer
-    const int scaling=6;
-    SDL_Window* window = nullptr;
-    SDL_Renderer* renderer = nullptr;
-    SDL_CreateWindowAndRenderer(n*scaling,n*scaling,0,&window,&renderer);
-    SDL_RenderSetScale(renderer,scaling,scaling);
-    //sorting
+    // std::shuffle(v.begin(), v.end(), rng);
 
-    for(int i= 1; i< v.size();i++){
-        for(int j = 0; j<v.size();j++){
-            if(v[i] < v[j]){
-                std::swap(v[i],v[j]);
-            }
-            //clear screen
-            SDL_SetRenderDrawColor(renderer,0,0,0,255);
-            SDL_RenderClear(renderer);
-            //Draw
-            draw(v,renderer,i,j);
-            //Show
-            SDL_RenderPresent(renderer);
-            SDL_Delay(1);
-        }
-    }
-    SDL_Delay(1);
-    for(int i = 0; i<v.size(); i++){
-        std::cout << v[i] << " ";
-    }
-
+    sorting.counting_sort(v,n,0);
+    sorting.print_array(v);
+    
+    //Returning
+    return 0;
 }
