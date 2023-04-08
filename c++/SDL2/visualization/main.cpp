594b10271d0e259e6286bf5f849d3c563f14e01e
Tristan Schiemenz <tristanschiemenz@gmail.com>
2023-04-08 18:25:07 +0200

    minor

--- c++/SDL2/visualization/main.cpp
+++ c++/SDL2/visualization/main.cpp
@@ -157,70 +157,73 @@ class SortingAlgs{
         }
         SDL_RenderPresent(renderer);
     }
-    void merge(std::vector<int> &array, int const left, int const mid, int const right,int delay)
-    {
-    auto const subArrayOne = mid - left + 1;
-    auto const subArrayTwo = right - mid;
- 
-    // Create temp arrays
-    auto *leftArray = new int[subArrayOne],
-         *rightArray = new int[subArrayTwo];
- 
-    // Copy data to temp arrays leftArray[] and rightArray[]
-    for (auto i = 0; i < subArrayOne; i++)
-        leftArray[i] = array[left + i];
-    for (auto j = 0; j < subArrayTwo; j++)
-        rightArray[j] = array[mid + 1 + j];
- 
-    auto indexOfSubArrayOne
-        = 0, // Initial index of first sub-array
-        indexOfSubArrayTwo
-        = 0; // Initial index of second sub-array
-    int indexOfMergedArray
-        = left; // Initial index of merged array
- 
-    // Merge the temp arrays back into array[left..right]
-    while (indexOfSubArrayOne < subArrayOne
-           && indexOfSubArrayTwo < subArrayTwo) {
-        if (leftArray[indexOfSubArrayOne]
-            <= rightArray[indexOfSubArrayTwo]) {
+    void merge(std::vector<int> &array, int const left, int const mid, int const right,int delay){
+        auto const subArrayOne = mid - left + 1;
+        auto const subArrayTwo = right - mid;
+    
+        // Create temp arrays
+        auto *leftArray = new int[subArrayOne],
+            *rightArray = new int[subArrayTwo];
+    
+        // Copy data to temp arrays leftArray[] and rightArray[]
+        for (auto i = 0; i < subArrayOne; i++)
+            leftArray[i] = array[left + i];
+        for (auto j = 0; j < subArrayTwo; j++)
+            rightArray[j] = array[mid + 1 + j];
+    
+        auto indexOfSubArrayOne
+            = 0, // Initial index of first sub-array
+            indexOfSubArrayTwo
+            = 0; // Initial index of second sub-array
+        int indexOfMergedArray
+            = left; // Initial index of merged array
+    
+        // Merge the temp arrays back into array[left..right]
+        while (indexOfSubArrayOne < subArrayOne
+            && indexOfSubArrayTwo < subArrayTwo) {
+            if (leftArray[indexOfSubArrayOne]
+                <= rightArray[indexOfSubArrayTwo]) {
+                array[indexOfMergedArray]
+                    = leftArray[indexOfSubArrayOne];
+                indexOfSubArrayOne++;
+            }
+            else {
+                array[indexOfMergedArray]
+                    = rightArray[indexOfSubArrayTwo];
+                indexOfSubArrayTwo++;
+            }
+            indexOfMergedArray++;
+            if(Sdrawing){
+                draw(array,indexOfMergedArray,indexOfSubArrayOne);
+                SDL_Delay(delay);
+            }
+        }
+        // Copy the remaining elements of
+        // left[], if there are any
+        while (indexOfSubArrayOne < subArrayOne) {
             array[indexOfMergedArray]
                 = leftArray[indexOfSubArrayOne];
             indexOfSubArrayOne++;
+            indexOfMergedArray++;
+            if(Sdrawing){
+                draw(array,indexOfMergedArray,indexOfSubArrayOne);
+                SDL_Delay(delay);
+            }
         }
-        else {
+        // Copy the remaining elements of
+        // right[], if there are any
+        while (indexOfSubArrayTwo < subArrayTwo) {
             array[indexOfMergedArray]
                 = rightArray[indexOfSubArrayTwo];
             indexOfSubArrayTwo++;
+            indexOfMergedArray++;
+            if(Sdrawing){
+                draw(array,indexOfMergedArray,indexOfSubArrayTwo);
+                SDL_Delay(delay);
+            }
         }
-        indexOfMergedArray++;
-        if(Sdrawing){
-            draw(array,indexOfMergedArray,indexOfSubArrayOne);
-            SDL_Delay(delay);
-        }
-    }
-    // Copy the remaining elements of
-    // left[], if there are any
-    while (indexOfSubArrayOne < subArrayOne) {
-        array[indexOfMergedArray]
-            = leftArray[indexOfSubArrayOne];
-        indexOfSubArrayOne++;
-        indexOfMergedArray++;
-        if(Sdrawing){
-            draw(array,indexOfMergedArray,indexOfSubArrayOne);
-            SDL_Delay(delay);
-        }
-    }
-    // Copy the remaining elements of
-    // right[], if there are any
-    while (indexOfSubArrayTwo < subArrayTwo) {
-        array[indexOfMergedArray]
-            = rightArray[indexOfSubArrayTwo];
-        indexOfSubArrayTwo++;
-        indexOfMergedArray++;
-    }
-    delete[] leftArray;
-    delete[] rightArray;
+        delete[] leftArray;
+        delete[] rightArray;
     }
     bool Sdrawing;
     SDL_Window* window = nullptr;
@@ -246,26 +249,26 @@ int main(int argc, char *argv[]){
     bool drawing = true;
     SortingAlgs sorting(drawing,6);
     
-    // //bubble
-    // sorting.bubble_sort(v,1);
+    //bubble
+    sorting.bubble_sort(v,1);
 
-    // std::shuffle(v.begin(), v.end(), rng);
-    // //Selction
-    // sorting.selection_sort(v,1);
+    std::shuffle(v.begin(), v.end(), rng);
+    //Selction
+    sorting.selection_sort(v,1);
 
-    // std::shuffle(v.begin(), v.end(), rng);
-    // //insert
-    // sorting.insertion_sort(v,1);
+    std::shuffle(v.begin(), v.end(), rng);
+    //insert
+    sorting.insertion_sort(v,1);
     
-    // std::shuffle(v.begin(), v.end(), rng);
-    // //merge
-    // sorting.merge_sort(v,0,n-1,0);
+    std::shuffle(v.begin(), v.end(), rng);
+    //merge
+    sorting.merge_sort(v,0,n-1,10);
 
-    // std::shuffle(v.begin(), v.end(), rng);
-    // //couning
-    // sorting.counting_sort(v,10);
+    std::shuffle(v.begin(), v.end(), rng);
+    //couning
+    sorting.counting_sort(v,10);
 
-    // sorting.print_array(v);
+    sorting.print_array(v);
     
     //Returning
     return 0;
