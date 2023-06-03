a795db89552364ece92d4b59b0fe19e14e4245d8
Tristan Schiemenz <tristanschiemenz@gmail.com>
2023-06-03 18:45:12 +0200

    adding pop, push, remove

--- c++/projects/LinkedLists/main.cpp
+++ c++/projects/LinkedLists/main.cpp
@@ -11,6 +11,26 @@ class LinkedList{
         head.poiItem = nullptr;
         head.poiNext = nullptr;
     };
+    void push(int value){
+        LinkedList_data_int* to_append = new LinkedList_data_int;
+        to_append->poiItem = new int;
+        *(to_append->poiItem) = value;
+
+        to_append->poiNext = head.poiNext;
+
+        
+        head.poiNext = to_append;
+    }
+    void pop(){
+        delete head.poiNext->poiItem;
+        head.poiNext->poiItem = nullptr;
+        LinkedList_data_int* next_pointer = head.poiNext->poiNext;
+        delete head.poiNext;
+        head.poiNext = next_pointer;
+
+    }
+
+    //adding at the end
     void append(int value){
         //Creating new struct
         LinkedList_data_int* to_append = new LinkedList_data_int;
@@ -26,7 +46,7 @@ class LinkedList{
             return;
         }
         
-        LinkedList_data_int* current = &head;
+        LinkedList_data_int* current = head.poiNext;
 
         //go to the one with nullpointer(last one)
         while(current->poiNext != nullptr){
@@ -36,6 +56,21 @@ class LinkedList{
         current->poiNext = to_append;
         //Memory Managment
         current = nullptr;
+    }
+    //removing the last item
+    void remove(){
+        
+        LinkedList_data_int* before_current = &head;
+        LinkedList_data_int* current = head.poiNext;
+        while(current->poiNext != nullptr){
+            before_current = current;
+            current = current->poiNext;
+        }
+
+        delete current->poiItem;
+        current->poiItem = nullptr;
+        delete current;
+        before_current->poiNext = nullptr;
     }
     int size(){
         LinkedList_data_int* current = &head;
@@ -58,11 +93,19 @@ class LinkedList{
 int main(){
     LinkedList list;
     int size = list.size();
-
     std::cout << size << "\n"; 
-    list.append(5);
-    list.append(5);
+    list.push(100);
+    list.append(100);
+    list.pop();
+    list.remove();
+    while(true){
+        list.push(100);
+        list.pop();
+    }
+    
     size = list.size();
+    std::cout << size;
+    
     std::cout << size;
 
 
