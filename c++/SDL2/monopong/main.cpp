355094beee1e5426be3220c7346941ccbff63e2c
Tristan Schiemenz <tristanschiemenz@gmail.com>
2023-07-01 16:35:07 +0200

    drawingBall funciton

--- c++/SDL2/monopong/main.cpp
+++ c++/SDL2/monopong/main.cpp
@@ -2,19 +2,36 @@
 #include <iostream>
 #include <cmath>
 #include <math.h>
+#include <vector>
 
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 const int RADIUS = 200;
+const int THICKNESS = 4;
 
-void DrawQuarterCircle(SDL_Renderer* renderer, int x, int y, int radius, double startAngle) {
-    double endAngle = startAngle + M_PI / 2;
-    for (double theta = startAngle; theta < endAngle; theta += 0.01) {
-        int xPos = x + radius * cos(theta);
-        int yPos = y + radius * sin(theta);
-        SDL_RenderDrawPoint(renderer, xPos, yPos);
+
+void DrawQuarterCircle(SDL_Renderer* renderer, int x, int y, int radius, double startAngle, int thick, std::vector<std::pair<int,int>> * positions) {
+    double endAngle = startAngle + 2 * M_PI / 7;
+    for(int i = 0; i<thick; i++){
+        for (double theta = startAngle; theta < endAngle; theta += 0.01) {
+            int xPos = x + radius * cos(theta);
+            int yPos = y + radius * sin(theta);
+            positions->push_back(std::pair<int,int>(xPos, yPos));
+            SDL_RenderDrawPoint(renderer, xPos, yPos);
+        }
+        radius++;
     }
+}
+void DrawBall(SDL_Renderer* renderer, int * x ,int * y, int xaccel, int yaccel){
+    SDL_Rect rect;
+    rect.x = *x;
+    rect.y = *y;
+    rect.w = THICKNESS*2;
+    rect.h = THICKNESS*2;
+    SDL_RenderDrawRect(renderer, &rect);
+    *x += xaccel;
+    *y += yaccel;
 }
 
 int main(int argc, char* argv[]) {
@@ -26,6 +43,10 @@ int main(int argc, char* argv[]) {
     double startAngle = 0.0;
     SDL_Event e;
     bool quit = false;
+    int xBall = SCREEN_WIDTH/2;
+    int yBall = SCREEN_HEIGHT/2;
+    int xac = 1;
+    int yac = -1;
 
     while (!quit) {
         while (SDL_PollEvent(&e) != 0) {
@@ -35,10 +56,10 @@ int main(int argc, char* argv[]) {
             else if(e.type == SDL_KEYDOWN){
                 switch(e.key.keysym.sym){
                     case SDLK_LEFT:
-                        startAngle += 0.1; // Erhöht den Winkel um 0.01 Radian
+                        startAngle += 0.05; // Erhöht den Winkel um 0.01 Radian
                         break;
                     case SDLK_RIGHT:
-                        startAngle -= 0.1; // Verringert den Winkel um 0.01 Radian
+                        startAngle -= 0.05; // Verringert den Winkel um 0.01 Radian
                         break;
                 }
             }
@@ -54,9 +75,16 @@ int main(int argc, char* argv[]) {
         SDL_RenderClear(renderer);
 
         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-        DrawQuarterCircle(renderer, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, RADIUS, startAngle);
+
+        std::vector<std::pair<int,int>> circlePos;
+
+        DrawQuarterCircle(renderer, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, RADIUS, startAngle,THICKNESS,&circlePos);
+
+        DrawBall(renderer,&xBall,&yBall, xac, yac);
 
         SDL_RenderPresent(renderer);
+
+        circlePos.clear();
     }
 
     SDL_DestroyRenderer(renderer);
